2a954ab01c63839f20c56545c500ea282a2d201a
DEMENSDEUM-ACER\Demensdeum <demensdeum@gmail.com>
2025-08-21 22:38:44 +0400

    Translation

--- resources/translation/raidenvideoripper_ru.ts
+++ resources/translation/raidenvideoripper_ru.ts
@@ -4,127 +4,162 @@
 <context>
     <name>EditorWindow</name>
     <message>
-        <location filename="../../editorwindow.cpp" line="50"/>
+        <location filename="../../editorwindow.cpp" line="53"/>
         <source>Video (mp4)</source>
         <translation>Видео (mp4)</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="71"/>
+        <location filename="../../editorwindow.cpp" line="61"/>
+        <source>Animation (Gif)</source>
+        <translation>Анимация (Gif)</translation>
+    </message>
+    <message>
+        <location filename="../../editorwindow.cpp" line="69"/>
+        <source>Animation (webp)</source>
+        <translation>Анимация (webp)</translation>
+    </message>
+    <message>
+        <location filename="../../editorwindow.cpp" line="77"/>
+        <source>Video (WebM)</source>
+        <translation type="unfinished">Видео (WebM)</translation>
+    </message>
+    <message>
+        <location filename="../../editorwindow.cpp" line="85"/>
+        <source>Video (ogv)</source>
+        <translation>Видео (ogv)</translation>
+    </message>
+    <message>
+        <location filename="../../editorwindow.cpp" line="97"/>
         <source>Audio (mp3)</source>
         <translation>Аудио (mp3)</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="150"/>
+        <location filename="../../editorwindow.cpp" line="107"/>
+        <source>Audio (wav)</source>
+        <translation>Аудио (wav)</translation>
+    </message>
+    <message>
+        <location filename="../../editorwindow.cpp" line="120"/>
+        <source>Audio (ogg)</source>
+        <translation>Аудио (ogg)</translation>
+    </message>
+    <message>
+        <location filename="../../editorwindow.cpp" line="186"/>
         <source>Open...</source>
         <translation>Открыть...</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="154"/>
+        <location filename="../../editorwindow.cpp" line="190"/>
         <source>Exit</source>
         <translation>Выход</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="157"/>
+        <location filename="../../editorwindow.cpp" line="193"/>
         <source>&amp;File</source>
         <translation>&amp;Файл</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="161"/>
+        <location filename="../../editorwindow.cpp" line="197"/>
         <source>&amp;Options</source>
         <translation>&amp;Опции</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="164"/>
+        <location filename="../../editorwindow.cpp" line="200"/>
         <source>Preview</source>
         <translation>Предосмотр</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="189"/>
+        <location filename="../../editorwindow.cpp" line="225"/>
         <source>&amp;About</source>
         <translation>&amp;Справка</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="191"/>
+        <location filename="../../editorwindow.cpp" line="227"/>
         <source>About Raiden Video Ripper</source>
         <translation>Raiden Video Ripper</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="195"/>
+        <location filename="../../editorwindow.cpp" line="231"/>
         <source>About &amp;Qt</source>
         <translation>&amp;Qt</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="299"/>
+        <location filename="../../editorwindow.cpp" line="335"/>
         <source>START</source>
         <translation>НАЧАТЬ</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="565"/>
-        <source>Copyright &amp;copy; 2023 &lt;a href=&quot;https://www.demensdeum.com/&quot;&gt;Ilia Prokhorov&lt;/a&gt;</source>
-        <translation>Copyright &amp;copy; 2023 &lt;a href=&quot;https://www.demensdeum.com/&quot;&gt;Илья Прохоров&lt;/a&gt;</translation>
+        <location filename="../../editorwindow.cpp" line="619"/>
+        <source>Copyright &amp;copy; 2025 &lt;a href=&quot;https://www.demensdeum.com/&quot;&gt;I/E Ilia Prokhorov&lt;/a&gt;</source>
+        <translation>Copyright &amp;copy; 2025 &lt;a href=&quot;https://www.demensdeum.com/&quot;&gt;I/E Ilia Prokhorov&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="574"/>
+        <location filename="../../editorwindow.cpp" line="1001"/>
+        <source>Error while cutting! Result code: %1</source>
+        <translation>Ошибка во время обработки! Код: %1</translation>
+    </message>
+    <message>
+        <source>Copyright &amp;copy; 2023 &lt;a href=&quot;https://www.demensdeum.com/&quot;&gt;Ilia Prokhorov&lt;/a&gt;</source>
+        <translation type="vanished">Copyright &amp;copy; 2023 &lt;a href=&quot;https://www.demensdeum.com/&quot;&gt;Илья Прохоров&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../editorwindow.cpp" line="628"/>
         <source>About %1</source>
         <translation>О приложении %1</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="575"/>
+        <location filename="../../editorwindow.cpp" line="629"/>
         <source>&lt;h1&gt;Version %1 %2&lt;/h1&gt;&lt;p&gt;&lt;a href=&quot;%3&quot;&gt;%1&lt;/a&gt; is an open-source project designed for video editing and format conversion. It&apos;s built using Qt 6 (Qt Creator) and allows you to trim and convert videos to MP4 or GIF formats.&lt;/p&gt;&lt;small&gt;&lt;p&gt;%4&lt;/p&gt;&lt;p&gt;Licensed under the %5&lt;/p&gt;&lt;p&gt;This program proudly uses the following projects:&lt;ul&gt;&lt;li&gt;&lt;a href=&quot;https://www.qt.io/&quot;&gt;Qt&lt;/a&gt; application and UI framework&lt;/li&gt;&lt;li&gt;&lt;a href=&quot;https://www.ffmpeg.org/&quot;&gt;FFmpeg&lt;/a&gt; multimedia format and codec libraries&lt;/li&gt;&lt;li&gt;&lt;a href=&quot;https://github.com/demensdeum/Dullahan-FFmpeg/&quot;&gt;Dullahan-FFmpeg&lt;/a&gt; FFmpeg CLI as shared library &lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;p&gt;The source code used to build this program can be downloaded from %6&lt;/p&gt;This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.&lt;/small&gt;</source>
         <translation>&lt;h1&gt;Версия %1 %2&lt;/h1&gt;&lt;p&gt;&lt;a href=&quot;%3&quot;&gt;%1&lt;/a&gt; - это проект с открытым исходным кодом, разработанный для видеомонтажа и конвертации форматов. Он создан с использованием Qt 6 (Qt Creator) и позволяет обрезать и конвертировать видео в форматы MP4 или GIF.&lt;/p&gt;&lt;small&gt;&lt;p&gt;%4&lt;/p&gt;&lt;p&gt;Лицензируется в соответствии с %5&lt;/p&gt;&lt;p&gt;Эта программа с гордостью использует следующие проекты:&lt;ul&gt;&lt;li&gt;&lt;a href=&quot;https://www.qt.io/&quot;&gt;Qt&lt;/a&gt; - приложение и фреймворк для пользовательского интерфейса&lt;/li&gt;&lt;li&gt;&lt;a href=&quot;https://www.ffmpeg.org/&quot;&gt;FFmpeg&lt;/a&gt; - мультимедийные библиотеки форматов и кодеков&lt;/li&gt;&lt;li&gt;&lt;a href=&quot;https://github.com/demensdeum/Dullahan-FFmpeg/&quot;&gt;Dullahan-FFmpeg&lt;/a&gt; - FFmpeg CLI в виде общей библиотеки&lt;/li&gt;&lt;/ul&gt;&lt;p&gt;Исходный код, использованный для создания этой программы, можно скачать по ссылке %6&lt;/p&gt;&lt;p&gt;Эта программа распространяется с надеждой, что она будет полезной, но БЕЗ КАКИХ-ЛИБО ГАРАНТИЙ; даже без подразумеваемой гарантии ПРИГОДНОСТИ ДЛЯ ОПРЕДЕЛЕННЫХ ЦЕЛЕЙ.&lt;/p&gt;&lt;/small&gt;</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="751"/>
-        <location filename="../../editorwindow.cpp" line="756"/>
+        <location filename="../../editorwindow.cpp" line="799"/>
+        <location filename="../../editorwindow.cpp" line="804"/>
         <source>WUT!</source>
         <translation>Что??!</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="751"/>
+        <location filename="../../editorwindow.cpp" line="799"/>
         <source>Open file first!</source>
         <translation>Откройте файл сначала!</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="756"/>
+        <location filename="../../editorwindow.cpp" line="804"/>
         <source>Select output formats checkboxes first!</source>
         <translation>Выберите выходные форматы в опциях!</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="865"/>
+        <location filename="../../editorwindow.cpp" line="922"/>
         <source>Cutting </source>
         <translation>Монтаж </translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="931"/>
         <source>Error while cutting! Result code: %1
 Output files:
 %2</source>
-        <translation>Ошибка монтажа! Код возврата: %1
+        <translation type="vanished">Ошибка монтажа! Код возврата: %1
 Выходные файлы:
 %2</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="120"/>
         <source>Wow!</source>
-        <translation>Ухты!</translation>
+        <translation type="vanished">Ухты!</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="111"/>
         <source>Output files:&lt;br&gt;%1</source>
-        <translation>Выходные файлы:&lt;br&gt;%1</translation>
+        <translation type="vanished">Выходные файлы:&lt;br&gt;%1</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="114"/>
         <source>Success!</source>
-        <translation>Всё получилось!</translation>
+        <translation type="vanished">Всё получилось!</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="116"/>
         <source>&lt;b&gt;Success!&lt;/b&gt;&lt;br&gt;If you like this application, please consider a donation:&lt;br&gt;&lt;a href=&quot;https://www.donationalerts.com/r/demensdeum&quot;&gt;https://www.donationalerts.com/r/demensdeum&lt;/a&gt;</source>
-        <translation>&lt;b&gt;Успех!&lt;/b&gt;&lt;br&gt;Если вам нравится это приложение, пожалуйста, рассмотрите возможность сделать пожертвование по ссылке:&lt;br&gt;&lt;a href=&quot;https://www.donationalerts.com/r/demensdeum&quot;&gt;https://www.donationalerts.com/r/demensdeum&lt;/a&gt;</translation>
+        <translation type="vanished">&lt;b&gt;Успех!&lt;/b&gt;&lt;br&gt;Если вам нравится это приложение, пожалуйста, рассмотрите возможность сделать пожертвование по ссылке:&lt;br&gt;&lt;a href=&quot;https://www.donationalerts.com/r/demensdeum&quot;&gt;https://www.donationalerts.com/r/demensdeum&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="../../editorwindow.cpp" line="930"/>
+        <location filename="../../editorwindow.cpp" line="1000"/>
+        <location filename="../../editorwindow.cpp" line="1078"/>
         <source>Uhh!</source>
         <translation>Ой!</translation>
     </message>
@@ -132,9 +167,32 @@ Output files:
 <context>
     <name>ProgressBarWindow</name>
     <message>
-        <location filename="../../progressbarwindow.cpp" line="19"/>
+        <location filename="../../progressbarwindow.cpp" line="24"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Отмена</translation>
+    </message>
+</context>
+<context>
+    <name>SuccessWindow</name>
+    <message>
+        <location filename="../../successwindow.cpp" line="11"/>
+        <source>Success</source>
+        <translation>Успех</translation>
+    </message>
+    <message>
+        <location filename="../../successwindow.cpp" line="22"/>
+        <source>All files were successfully cut.</source>
+        <translation>Все файлы были успешно обработаны.</translation>
+    </message>
+    <message>
+        <location filename="../../successwindow.cpp" line="23"/>
+        <source>Click here to open the location.</source>
+        <translation>Нажмите для открытия папки.</translation>
+    </message>
+    <message>
+        <location filename="../../successwindow.cpp" line="30"/>
+        <source>OK</source>
+        <translation>ОК</translation>
     </message>
 </context>
 </TS>
